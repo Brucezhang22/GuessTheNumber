21b506dcc20acd089629072f08a846557dba6300
MudOnTire <895157882@qq.com>
2017-10-21 10:29:07 +0800

    修改代码

--- main.js
+++ main.js
@@ -1,61 +1,91 @@
-(function () {
-
+(function() {
 	var randomNum;
 	var guessCount;
+	var gameOver;
 	var numInput = document.querySelector(".num-input");
 	var resultP = document.querySelector(".result");
+	var historyUl = document.querySelector(".history");
 
-	Element.prototype.on = function(type, listener){
+	Element.prototype.on = function(type, listener) {
 		var element = this;
 		if (document.addEventListener) {
 			element.addEventListener(type, listener);
-		}else if(document.attachEvent){
-			element.attachEvent("on"+type, listener);
-		}else{
-			element["on"+type]=listener;
+		} else if (document.attachEvent) {
+			element.attachEvent("on" + type, listener);
+		} else {
+			element["on" + type] = listener;
 		}
-	}
+	};
 
-	function getRandomNumber(min, max){
+	function getRandomNumber(min, max) {
 		var num = Math.round(Math.random() * (max - min) + min);
 		console.log(num);
 		return num;
 	}
 
-	function guess(e){
-		var inputNum = numInput.value;
-		console.log(inputNum);
-		var resultTxt;
-		if (guessCount < 10) {
-			if (inputNum < randomNum) {
-				resultTxt = "Oops, it's a bit small";
-			}else if (inputNum > randomNum) {
-				resultTxt = "Oops, it's a bit large";
-			}else{
-				resultTxt = "Well done! You got me!";
+	function addHistory(item){
+		var li = document.createElement("li");
+		li.innerText = item;
+		historyUl.appendChild(li);
+	}
+
+	function guess(e) {
+		if (gameOver) {
+			var restart = confirm("do you want to play again?");
+			if (restart) {
+				reset();
 			}
-		}else{
-			resultTxt = "Game over, you've used up your chance!";
+			return;
+		}
+		var inputNum = numInput.value;
+		var resultTxt;
+		console.log(inputNum);
+		if (inputNum.length <= 0) {
+			resultTxt = "Please enter a valid number first!";
+		} else {
+			if (guessCount < 10) {
+				if (inputNum < randomNum) {
+					resultTxt = "Oops, it's a bit small";
+				} else if (inputNum > randomNum) {
+					resultTxt = "Oops, it's a bit large";
+				} else {
+					resultTxt = "Well done! You got me!";
+					gameOver = true;
+				}
+				addHistory(inputNum);
+			} else {
+				resultTxt = "Game over, you've used up your chance!";
+				gameOver = true;
+			}
 		}
 		resultP.innerText = resultTxt;
 	}
 
-	function reset(){
-		randomNum = getRandomNumber(0,100);
+	function reset() {
+		randomNum = getRandomNumber(0, 100);
 		guessCount = 0;
 		resultP.innerText = "Please enter a number!";
+		numInput.value = null;
+		historyUl.innerHTML = "";
+		gameOver = false;
 	}
 
-	function initial(){
+	function initial() {
 		reset();
 		var startBtn = document.querySelector(".btn-start");
 		var guessBtn = document.querySelector(".btn-guess");
-		startBtn.on("click", function(e){
+		numInput.on("keyup", function(e){
+			var keyCode = e.keyCode || e.which;
+			if (keyCode === 13) {
+				guess();
+				this.value = null;
+			}
+		})
+		startBtn.on("click", function(e) {
 			reset();
 		});
 		guessBtn.on("click", guess);
 	}
 
 	initial();
-
 })();
